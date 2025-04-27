be92f0f2c622e00319fe4a5ecc5401637b1a4f4a
namonaki1234 <123581321fns1119@gmail.com>
2025-04-27 10:55:59 +0900

    create toppage

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/index.css'
-import "./styles/destyle.css"
+// import "./styles/destyle.css"
+import { TopPage } from './components/pages/TopPage'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    
+    <TopPage />
   </StrictMode>,
 )
