a42dce9aa3ef67506837ea035579e10299ba466d
namonaki1234 <123581321fns1119@gmail.com>
2025-04-29 17:29:27 +0900

    feat:create login page

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,3 +1,28 @@
+import { PrimaryButton } from '../atoms/PrimaryButton';
+
 export const LoginPage = () => {
-  return <div>LoginPage</div>;
+  return (
+    <div className="w-[500px] bg-white rounded-lg py-10">
+      <form className="flex flex-col items-center justify-center gap-10">
+        <h1 className="text-3xl font-bold text-lime-800 text-center">
+          ログイン
+        </h1>
+        <div className="w-[80%]">
+          <input
+            type="text"
+            placeholder="email"
+            className="w-full border-4 border-solid border-lime-800 rounded-md p-2"
+          />
+        </div>
+        <div className="w-[80%]">
+          <input
+            type="text"
+            placeholder="password"
+            className="w-full border-4 border-solid border-lime-800 rounded-md p-2"
+          />
+        </div>
+        <PrimaryButton>ログイン</PrimaryButton>
+      </form>
+    </div>
+  );
 };
--- src/routes.tsx
+++ src/routes.tsx
@@ -2,7 +2,7 @@ import { createBrowserRouter } from 'react-router-dom';
 import { TopPage } from './components/pages/TopPage';
 import { LoginPage } from './components/pages/LoginPage';
 import { NotLoginLayout } from './components/templates/NotLoginLayout';
-
+    
 export const router = createBrowserRouter([
   {
     path: '/',
